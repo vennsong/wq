55c87a2cf590e488ad2590c7b672cd6c26b908e0
songwenqiang <wenqiang.song@medbanks.cn>
2018-07-12 13:22:56 +0800

    2

--- ss.sh
+++ ss.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-
+key=""
 case $1 in
   install)
-    wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
-    tar xf libsodium-1.0.16.tar.gz && cd libsodium-1.0.16
+    apt-get install make gcc libev-dev libc-ares-dev libpcre3 libpcre3-dev rng-tools
+
+    wget https://github.com/jedisct1/libsodium/releases/download/1.0.12/libsodium-1.0.12.tar.gz
+    tar xf libsodium-1.0.12.tar.gz && cd libsodium-1.0.12
     ./configure && make -j2 && make install
     ldconfig
+
+    cd /root
     
     wget https://github.com/xtaci/kcptun/releases/download/v20180316/kcptun-linux-amd64-20180316.tar.gz
     tar xf kcptun-linux-amd64-20180316.tar.gz
@@ -16,17 +20,21 @@ case $1 in
     
     wget https://github.com/vennsong/wq/releases/download/shadowsocks/91yunserverspeeder.tar.gz
   ;;
+  kernel)
+    wget http://security-cdn.debian.org/pool/updates/main/l/linux/linux-image-3.16.0-4-amd64_3.16.43-2+deb8u5_amd64.deb
+    dpkg -i linux-image-3.16.0-4*.deb
+    update-grub
+  ;;
   start)
-    /root/ss-server -s 0.0.0.0 -p 443 -l 1080 -m 'chacha20-ietf-poly1305' -k 'yourkey' \
+    /root/ss-server -s 0.0.0.0 -p 443 -l 1080 -m 'chacha20-ietf-poly1305' -k "$key" \
        > /dev/null 2>&1 &
     
-    key='yourkey'
     /root/server_linux_amd64 \
       -t "127.0.0.1:443" \
       -l ":443" \
       --key $key \
       --mode fast2 \
-      --crypt xor \
+      --crypt aes-128 \
       --quiet > /dev/null 2>&1 &
   ;;
   *)
